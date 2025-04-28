7b76fd2a550d87385cac698350fb652ae6bf6bc8
Robert <3luminox3@gmail.com>
2025-04-28 17:51:21 +0200

    constraint class for one week

---
+++ backend/models/Constraints.py
@@ -0,0 +1,13 @@
+class Constraints():
+
+    def __init__(self):
+        self.days = 7
+        self.shifts = 2
+        self.num_managers = 1
+        self.num_cashiers = 3
+        self.num_forklifts = 1
+        self.num_coordinators = 1
+        self.min_num_workers = 6
+        self.max_num_workers = 7
+        # wartości na podstawie rozmów z klienetem
+
