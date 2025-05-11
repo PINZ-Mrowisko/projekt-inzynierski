81b0c5b1541f4b9010c5b0c5abca105fb5d30b47
Robert <3luminox3@gmail.com>
2025-05-11 13:23:49 +0200

    more employees, creating all possible shifts configurations

--- backend/algorithm/algorithm.py
+++ backend/algorithm/algorithm.py
@@ -19,23 +19,53 @@ def main():
     cashier1 = Worker("Adam", "Mada", 20, "umowa zlecenie", 222, "123@wp.pl")
     cashier2 = Worker("Bartek", "Ketrab", 20, "umowa zlecenie", 222, "123@wp.pl")
     cashier3 = Worker("Czesław", "Wałsecz", 20, "umowa zlecenie", 222, "123@wp.pl")
+    cashier4 = Worker("Alan", "Nala", 20, "umowa zlecenie", 222, "123@wp.pl")
+    cashier5 = Worker("Barbara", "Arabrab", 20, "umowa zlecenie", 222, "123@wp.pl")
+    cashier6 = Worker("Cecylia", "Ailycec", 20, "umowa zlecenie", 222, "123@wp.pl")
 
     cashier1.add_tag(kasjer)
     cashier2.add_tag(kasjer)
     cashier3.add_tag(kasjer)
+    cashier4.add_tag(kasjer)
+    cashier5.add_tag(kasjer)
+    cashier6.add_tag(kasjer)
 
-    wozkowy = Worker("Damian", "Naimad", 20, "umowa zlecenie", 222, "123@wp.pl")
-    wozkowy.add_tag(wozek_widlowy)
+    wozkowy1 = Worker("Damian", "Naimad", 20, "umowa zlecenie", 222, "123@wp.pl")
+    wozkowy2 = Worker("Duda", "Adud", 20, "umowa zlecenie", 222, "123@wp.pl")
+    wozkowy3 = Worker("Dagmara", "Aramgad", 20, "umowa zlecenie", 222, "123@wp.pl")
 
-    manager = Worker("Edyta", "Atyde", 20, "umowa zlecenie", 222, "123@wp.pl")
-    manager.add_tag(kierownik)
+    wozkowy1.add_tag(wozek_widlowy)
+    wozkowy2.add_tag(wozek_widlowy)
+    wozkowy3.add_tag(wozek_widlowy)
 
-    coordinator = Worker("Felix", "Xilef", 20, "umowa zlecenie", 222, "123@wp.pl")
-    coordinator.add_tag(koordynator)
+    manager1 = Worker("Edyta", "Atyde", 20, "umowa zlecenie", 222, "123@wp.pl")
+    vice_manager1 = Worker("Eliasz", "Zsaile", 20, "umowa zlecenie", 222, "123@wp.pl")
+    vice_manager2 = Worker("Edward", "Drawde", 20, "umowa zlecenie", 222, "123@wp.pl")
 
-    workers = [cashier1, cashier2, cashier3, wozkowy, manager, coordinator]
+    manager1.add_tag(kierownik)
+    vice_manager1.add_tag(kierownik)
+    vice_manager2.add_tag(kierownik)
+
+    coordinator1 = Worker("Felix", "Xilef", 20, "umowa zlecenie", 222, "123@wp.pl")
+    coordinator2 = Worker("Fiona", "Anoif", 20, "umowa zlecenie", 222, "123@wp.pl")
+
+    coordinator1.add_tag(koordynator)
+    coordinator2.add_tag(koordynator)
+
+    workers = [cashier1, cashier2, cashier3, cashier4, cashier5, cashier6, wozkowy1, wozkowy2, wozkowy3, manager1,
+               vice_manager1, vice_manager2, coordinator1, coordinator2]
+
+    all_workers = len(workers)
+    print("Number of workers: ", all_workers)
+
+    all_shifts = {}
+
     for worker in workers:
-        print(worker)
+        for day in range(constraints.days):
+            for shift in range(constraints.shifts):
+                all_shifts[(worker, day, shift)] = model.new_bool_var(f"shift_n{worker}_d{day}_{shift}")
+
+
 
 
 if __name__ == "__main__":
