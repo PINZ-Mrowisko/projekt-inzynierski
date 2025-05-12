a5f4f2613813d9b90c0a4f9fc696cd9effbcc45a
Robert <3luminox3@gmail.com>
2025-05-12 13:14:21 +0200

    rules for different roles attendance

--- backend/algorithm/algorithm.py
+++ backend/algorithm/algorithm.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from ortools.sat.python import cp_model
 
 from backend.models.Constraints import Constraints
@@ -24,6 +26,7 @@ def main():
     cashier6 = Worker("Cecylia", "Ailycec", 20, "umowa zlecenie", 222, "123@wp.pl")
 
     cashier1.add_tag(kasjer)
+    cashier1.add_tag(wozek_widlowy)
     cashier2.add_tag(kasjer)
     cashier3.add_tag(kasjer)
     cashier4.add_tag(kasjer)
@@ -55,6 +58,11 @@ def main():
     workers = [cashier1, cashier2, cashier3, cashier4, cashier5, cashier6, wozkowy1, wozkowy2, wozkowy3, manager1,
                vice_manager1, vice_manager2, coordinator1, coordinator2]
 
+    cashiers = [c for c in workers if kasjer in c.tags]
+    wozkowicze = [w for w in workers if wozek_widlowy in w.tags]
+    coordiantors = [c for c in workers if koordynator in c.tags]
+    managers = [m for m in workers if kierownik in m.tags]
+
     all_workers = len(workers)
     print("Number of workers: ", all_workers)
 
@@ -63,9 +71,40 @@ def main():
     for worker in workers:
         for day in range(constraints.days):
             for shift in range(constraints.shifts):
-                all_shifts[(worker, day, shift)] = model.new_bool_var(f"shift_n{worker}_d{day}_{shift}")
+                for role in worker.tags:
+                    all_shifts[(worker, day, shift, role)] = model.new_bool_var(f"shift: {worker} | {day} | {shift} | {role}")
 
+    # for day in range(constraints.days):
+    #     for shift in range(constraints.shifts):
+    #         model.add(all_shifts[(cashier, day, shift, role)] for cashier in cashiers)
 
+    for day in range(constraints.days):
+        for shift in range(constraints.shifts):
+            # Sum of cashiers working in 'kasjer' role for this shift/day
+
+            cashier_assignments = [
+                all_shifts[(cashier, day, shift, kasjer)]
+                for cashier in cashiers
+            ]
+
+            model.Add(sum(cashier_assignments) == 3)
+
+            wozkowicze_assignments = [
+                all_shifts[(wozkowicz, day, shift, wozek_widlowy)]
+                for wozkowicz in wozkowicze
+
+            ]
+
+            model.Add(sum(wozkowicze_assignments) >= 1)
+
+            # coordinator_assignments = [
+            #     all_shifts[(coordinator, day, shift, koordynator)]
+            #     for coordinator in coordiantors
+            # ]
+
+            model.add_exactly_one(all_shifts[coordinator, day, shift, koordynator] for coordinator in coordiantors)
+
+            model.add_exactly_one(all_shifts[manager, day, shift, kierownik] for manager in managers)
 
 
 if __name__ == "__main__":
