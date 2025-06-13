db84b37978c5b9443562a565eef1fe8161a01902
Robert <3luminox3@gmail.com>
2025-06-13 15:31:05 +0200

    importing firebase SDK, testing connection succesful

---
+++ backend/connection/connection.py
@@ -0,0 +1,16 @@
+import firebase_admin
+from firebase_admin import firestore, credentials
+
+cred = credentials.Certificate("../ServiceAccountKey.json")
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client(app)
+doc_ref = db.collection("Markets")
+
+try:
+    docs = doc_ref.get()
+    for doc in docs:
+        print(f"{doc.id} => {doc.to_dict()}")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
