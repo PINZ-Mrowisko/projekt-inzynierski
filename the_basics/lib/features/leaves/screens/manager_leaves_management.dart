e2120face669983e17a930eff8b2f83e361c8366
juliaosinska <julka.osinska2003@gmail.com>
2025-06-19 16:01:27 +0200

    pop-up widths fixed, changed first day of the week on date picker to monday

--- the_basics/lib/features/leaves/screens/manager_leaves_management.dart
+++ the_basics/lib/features/leaves/screens/manager_leaves_management.dart
@@ -119,8 +119,8 @@ class ManagerLeavesManagementPage extends StatelessWidget {
         },
         hintText: 'Filtruj po pracowniku',
         leadingIcon: Icons.filter_alt_outlined,
-        widthPercentage: 0.2,
-        maxWidth: 360,
+        widthPercentage: 0.3,
+        maxWidth: 450,
         minWidth: 160,
       );
     });
