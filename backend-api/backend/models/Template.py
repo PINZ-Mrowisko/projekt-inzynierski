d1b28a60b660a3bb61b1e2a0e0904d74177fa06b
Robert <3luminox3@gmail.com>
2025-11-06 16:54:21 +0100

    simple template class

---
+++ backend-api/backend/models/Template.py
@@ -0,0 +1,11 @@
+class Template:
+
+    def __init__(self, id, isDataMising, isDeleted, maxMen, maxWomen, minMen, minWomen):
+
+        self.id = id
+        self.isDataMising = isDataMising
+        self.isDeleted = isDeleted
+        self.maxMen = maxMen
+        self.maxWomen = maxWomen
+        self.minMen = minMen
+        self.minWomen = minWomen
