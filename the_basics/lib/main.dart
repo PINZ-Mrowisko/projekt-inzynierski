744e62f7b19bb8b6c88da59a100f9bf77c6f2ada
juliaosinska <julosi@st.amu.edu.pl>
2025-12-03 00:39:31 +0100

    added user profile page

--- the_basics/lib/main.dart
+++ the_basics/lib/main.dart
@@ -24,6 +24,7 @@ import 'package:the_basics/features/schedules/screens/after_login/web/placeholde
 import 'package:the_basics/features/settings/screens/settings.dart';
 import 'package:the_basics/features/templates/screens/all_templates_screen.dart';
 import 'package:the_basics/features/templates/screens/new_template_screen.dart';
+import 'package:the_basics/features/user_profile/screens/web/user_profile.dart';
 import 'package:the_basics/utils/app_colors.dart';
 import 'package:the_basics/utils/bindings/app_bindings.dart';
 import 'package:the_basics/utils/common_widgets/bottom_menu_mobile/employee_more_page_mobile.dart';
@@ -108,7 +109,7 @@ class MyApp extends StatelessWidget {
         GetPage(name: '/wnioski-urlopowe-kierownik', page: () => PlatformWrapper(mobile: ManagerLeavesManagementMobilePage(), web: ManagerLeavesManagementPage())),
         
         GetPage(name: '/gielda', page: () => PlaceholderPage()),
-        GetPage(name: '/twoj-profil', page: () => PlaceholderPage()),
+        GetPage(name: '/twoj-profil', page: () => UserProfileScreen()),
         GetPage(name: '/tagi', page: () => TagsPage()),
         GetPage(name: '/pracownicy', page: () => EmployeeManagementPage()),
         GetPage(name: '/szablony', page: () => TemplatesPage()),
