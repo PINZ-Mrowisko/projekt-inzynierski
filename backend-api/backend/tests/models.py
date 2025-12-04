758926224b2cb9b5c6c0cf5b20ebe7e157c53722
Robert <3luminox3@gmail.com>
2025-12-04 22:02:20 +0100

    tests for Worker models adjusted to new constructor

--- backend-api/backend/tests/models.py
+++ backend-api/backend/tests/models.py
@@ -5,20 +5,20 @@ from ..models.Tags import Tags
 class TestWorker(unittest.TestCase):
 
     def test_working_hours(self):
-        person1 = Worker("adam", "kowalski", 21, "umowa o prace", 606547766, "ellele@wp.pl")
-        person2 = Worker("adam", "kowalski", 21, "umowa zlecenie", 606547766, "ellele@wp.pl")
-        person3 = Worker("adam", "kowalski", 21, "umowa zlecenie", 606547766, "ellele@wp.pl", 20)
+        person1 = Worker("adam", "kowalski", "M", 21, "umowa o prace", 606547766, "ellele@wp.pl", 1)
+        person2 = Worker("adam", "kowalski", "M", 21, "umowa zlecenie", 606547766, "ellele@wp.pl", 2)
+        person3 = Worker("adam", "kowalski", "M", 21, "umowa zlecenie", 606547766, "ellele@wp.pl", 3, 20)
 
-        assert person1.get_max_working_hours() == 40
-        assert person2.get_max_working_hours() == None
-        assert person3.get_max_working_hours() == 20
+        self.assertEqual(person1.get_max_working_hours(), 40)
+        self.assertIsNone(person2.get_max_working_hours())
+        self.assertEqual(person3.get_max_working_hours(), 20)
 
     def test_worker_tags(self):
-        person1 = Worker("adam", "kowalski", 21, "umowa o prace", 606547766, "ellele@wp.pl")
-        tag1 = Tags("kierownik", "osoba zarządzająca")
-        tag2 = Tags("wozek", "osoba z uprawnieniami na wozek")
+        person1 = Worker("adam", "kowalski", "M", 21, "umowa o prace", 606547766, "ellele@wp.pl", 4)
+        tag1 = Tags(1,"kierownik", "osoba zarządzająca")
+        tag2 = Tags(2,"wozek", "osoba z uprawnieniami na wozek")
 
         person1.add_tag(tag1)
         person1.add_tag(tag2)
 
-        assert person1.tags == [tag1, tag2]
+        self.assertEqual(person1.tags, [tag1, tag2])
