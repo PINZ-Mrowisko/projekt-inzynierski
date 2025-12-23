d5a0de7cfecd685aa1df85d99a1217bd7978b664
luminoksik <lolowy3@interia.pl>
2025-12-23 21:52:53 +0100

    template tests

--- backend-api/backend/tests/test_models.py
+++ backend-api/backend/tests/test_models.py
@@ -1,6 +1,9 @@
 import unittest
+from unittest.mock import MagicMock
+
 from ..models.Worker import Worker
 from ..models.Tags import Tags
+from ..models.Template import Template
 
 class TestWorker(unittest.TestCase):
 
@@ -22,3 +25,22 @@ class TestWorker(unittest.TestCase):
         person1.add_tag(tag2)
 
         self.assertEqual(person1.tags, [tag1, tag2])
+
+class TestTemplate(unittest.TestCase):
+
+    def test_template_correct(self):
+
+        shift1 = MagicMock()
+        shift1.day = "Poniedziałek"
+        shifts = [shift1]
+
+
+        template = Template(1, 'dobry', 0, 1, 0, 1, shifts)
+
+        self.assertEqual(template.__str__(),"Template(id=1, description=dobry, maxMen=0, maxWomen=1, minMen=0, minWomen=1, shifts=1, days=7, shifts_number={'Poniedziałek': 1})")
+
+    def test_template_incorrect_no_shifts(self):
+
+        shifts = []
+        template = Template(1, 'niedobry', 0, 1, 0, 1, shifts)
+        self.assertEqual(template.__str__(), "Template(id=1, description=niedobry, maxMen=0, maxWomen=1, minMen=0, minWomen=1, shifts=0, days=7, shifts_number={})")
