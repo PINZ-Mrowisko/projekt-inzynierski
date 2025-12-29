87624bfb03dff6110fa70f75015328d32de1eeb7
juliaosinska <julka.osinska2003@gmail.com>
2025-12-29 01:30:03 +0100

    Revert "working sundays report real data fetch implemented"
    This reverts commit 72ea4ad1a83939485c1c27bd794430c40e3cc98b.

--- the_basics/macos/Flutter/GeneratedPluginRegistrant.swift
+++ the_basics/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,10 +11,8 @@ import firebase_auth
 import firebase_core
 import firebase_messaging
 import flutter_local_notifications
-import flutter_localization
 import flutter_secure_storage_macos
 import path_provider_foundation
-import printing
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -24,9 +22,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-  FlutterLocalizationPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalizationPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
