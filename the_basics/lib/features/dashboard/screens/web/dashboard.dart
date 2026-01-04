d9226ee9451b5de256ce7116f725462ec1b49e2d
juliaosinska <julka.osinska2003@gmail.com>
2026-01-04 03:32:49 +0100

    Revert "tags showing up as IDs fix and testng option added"
    This reverts commit fb792433009f5791076559aa298c02b895f84d85.

--- the_basics/lib/features/dashboard/screens/web/dashboard.dart
+++ the_basics/lib/features/dashboard/screens/web/dashboard.dart
@@ -5,7 +5,6 @@ import 'package:the_basics/features/dashboard/screens/web/dashboard_tiles/leaves
 import 'package:the_basics/features/dashboard/screens/web/dashboard_tiles/shift_tile.dart';
 import 'package:the_basics/features/leaves/controllers/leave_controller.dart';
 import 'package:the_basics/features/schedules/controllers/schedule_controller.dart';
-import 'package:the_basics/features/tags/controllers/tags_controller.dart';
 import 'package:the_basics/utils/app_colors.dart';
 import '../../../employees/controllers/user_controller.dart';
 import '../../../../utils/common_widgets/side_menu.dart';
@@ -21,7 +20,6 @@ class _ManagerDashboardScreenState extends State<ManagerDashboardScreen> {
   final UserController userController = Get.find<UserController>();
   final LeaveController leaveController = Get.find<LeaveController>();
   final SchedulesController schedulesController =Get.find<SchedulesController>();
-  final TagsController tagsController =Get.find<TagsController>();
 
   final isLoading = true.obs;
   final readyToShow = false.obs;
@@ -186,4 +184,422 @@ class _ManagerDashboardScreenState extends State<ManagerDashboardScreen> {
       );
     });
   }
+
+  // TILE WITH CURRENT SHIFT
+  Widget _shiftTile(UserController userController, SchedulesController schedulesController) {
+    final now = DateTime.now();
+    // use this to pass custom date to the datetime widget during testing
+    //final now = DateTime(2026, 1, 6, 10, 0);
+    final currentShifts = schedulesController.individualShifts
+        .where((shift) => isShiftNow(shift, now))
+        .toList()
+      ..sort((a, b) =>
+          a.employeeFirstName.compareTo(b.employeeFirstName));
+
+    
+    return _baseTile(
+      title: "Aktualna zmiana",
+      child: currentShifts.isEmpty
+        ? Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _buildDateTimeWidget(),
+              const SizedBox(height: 16),
+                Expanded(
+                  child: Center(
+                    child: Text("Brak aktywnej zmiany",
+                    style: TextStyle(color: AppColors.textColor2),
+                    ),
+                  ),
+                ),
+            ],
+          )
+        : Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildDateTimeWidget(),
+                const SizedBox(height: 16),
+                Expanded(
+                  child: GenericList<ScheduleModel>(
+                    items: currentShifts,
+                    itemBuilder: (context, shift) {
+                      return ListTile(
+                        contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 12,
+                        ),
+                        title: Text(
+                          '${shift.employeeFirstName} ${shift.employeeLastName}',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: AppColors.textColor1,
+                          ),
+                        ),
+                        // we show tags assigned by the algorithm (so as which tag (role) is the employee working current shift)
+                        subtitle: _buildEmployeeTags(_convertTagIdsToNames(shift.tags, tagsController)),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+    );
+  }
+
+  // for testing purposes - add an option to pass custom time to the widget
+  Widget _buildDateTimeWidget([DateTime? customNow]) {
+    return StreamBuilder<DateTime>(
+      stream: customNow == null 
+        ? Stream.periodic(const Duration(seconds: 1), (_) => DateTime.now())
+        : Stream.value(customNow),
+      builder: (context, snapshot) {
+        final now = snapshot.data ?? (customNow ?? DateTime.now());
+        final formattedDate = DateFormat('dd.MM.yyyy').format(now);
+        final formattedTime = DateFormat('HH:mm:ss').format(now);
+
+        return Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          decoration: BoxDecoration(
+            color: AppColors.lightBlue,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.access_time, size: 20, color: AppColors.logolighter),
+              const SizedBox(width: 8),
+              Text(
+                "$formattedDate, $formattedTime",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  color: AppColors.textColor1,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  bool isShiftNow(ScheduleModel shift, DateTime now) {
+    if (shift.shiftDate.year != now.year ||
+        shift.shiftDate.month != now.month ||
+        shift.shiftDate.day != now.day) {
+      return false;
+    }
+
+    final startMinutes = shift.start.hour * 60 + shift.start.minute;
+    final endMinutes = shift.end.hour * 60 + shift.end.minute;
+    final nowMinutes = now.hour * 60 + now.minute;
+
+    return nowMinutes >= startMinutes && nowMinutes < endMinutes;
+  }
+
+  List<String> _convertTagIdsToNames(List<String> tagIds, TagsController tagsController) {
+    final List<String> tagNames = [];
+    
+    for (final tagId in tagIds) {
+      try {
+        final foundTags = tagsController.allTags.where((t) => t.id == tagId).toList();
+        
+        if (foundTags.isNotEmpty) {
+          final tag = foundTags.first;
+          if (tag.tagName != null && tag.tagName!.isNotEmpty) {
+            tagNames.add(tag.tagName!);
+          } else {
+            tagNames.add(tagId);
+          }
+        } else {
+          tagNames.add(tagId);
+        }
+      } catch (e) {
+        tagNames.add(tagId);
+      }
+    }
+    
+    return tagNames;
+  }
+
+  // TILE WITH LEAVES TO APPROVE
+  Widget _leavesTile(LeaveController leaveController) {
+    final pending = leaveController.allLeaveRequests
+        .where((l) => l.status.toLowerCase() == 'oczekujący')
+        .toList()
+      ..sort((a, b) => b.startDate.compareTo(a.startDate));
+
+    return _baseTile(
+      title: "Wnioski urlopowe do rozpatrzenia",
+      child: pending.isEmpty
+          ? Center(
+              child: Text(
+                "Brak wniosków oczekujących",
+                style: TextStyle(color: AppColors.textColor2),
+              ),
+            )
+          : GenericList<LeaveModel>(
+                items: pending,
+                onItemTap: (leave) => Get.offNamed('/wnioski-urlopowe-kierownik'),
+                itemBuilder: (context, item) {
+                  final formattedDate = item.startDate == item.endDate
+                      ? DateFormat('dd.MM.yyyy').format(item.startDate)
+                      : '${DateFormat('dd.MM.yyyy').format(item.startDate)} - ${DateFormat('dd.MM.yyyy').format(item.endDate)}';
+
+                  return ListTile(
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 12,
+                    ),
+                    title: Text(
+                      (item.comment == "Brak komentarza" || item.comment == '')
+                          ? item.name
+                          : '${item.name} - ${item.comment}',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.textColor1,
+                      ),
+                    ),
+                    subtitle: Text(
+                      formattedDate,
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: AppColors.textColor2,
+                      ),
+                    ),
+                    trailing: _buildStatusChip(item.status),
+                  );
+                },
+              ),
+            
+    );
+  }
+
+  // TILE WITH SCHEDULE WARNINGS
+  Widget _importantTile() {
+    // hardcoded warnings for demonstration purposes, to implement proper warnings pull when schedules are available
+    final warnings = [
+      {
+        'title': 'Brakujące pokrycie',
+        'description': 'Zmiana poranna bez przypisanego pracownika',
+        'icon': Icons.warning,
+        'color': AppColors.warning,
+        'date': '15.12.2024',
+      },
+      {
+        'title': 'Brakujące pokrycie',
+        'description': 'Zmiana nocna bez przypisanego pracownika',
+        'icon': Icons.warning,
+        'color': AppColors.warning,
+        'date': '20.12.2024',
+      },
+      {
+        'title': 'Brakujące pokrycie',
+        'description': 'Zmiana popołudniowa bez przypisanego pracownika',
+        'icon': Icons.warning,
+        'color': AppColors.warning,
+        'date': 'Dzisiaj',
+      },
+    ];
+
+    return _baseTile(
+      title: "Ważne",
+      child: warnings.isEmpty
+          ? Center(
+                child: Text("Brak ostrzeżeń",
+                style: TextStyle(color: AppColors.textColor2),
+                ),
+              )
+          : GenericList<Map<String, dynamic>>(
+                items: warnings,
+                onItemTap: (warning) => Get.offNamed('/grafik-ogolny-kierownik'),
+                itemBuilder: (context, warning) {
+                  return ListTile(
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 12,
+                    ),
+                    leading: Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: warning['color'].withOpacity(0.1),
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        warning['icon'],
+                        size: 20,
+                        color: warning['color'],
+                      ),
+                    ),
+                    title: Text(
+                      warning['title'],
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.textColor1,
+                      ),
+                    ),
+                    subtitle: Text(
+                      warning['description'],
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: AppColors.textColor2,
+                      ),
+                    ),
+                    trailing: Text(
+                      warning['date'],
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.textColor2,
+                      ),
+                    ),
+                  );
+                },
+              ),
+          
+    );
+  }
+
+  // HELPERS
+  Widget _baseTile({required String title, Widget? child}) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.black.withOpacity(0.06),
+            blurRadius: 6,
+            spreadRadius: 1,
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: AppColors.logolighter,
+            ),
+          ),
+
+          if (child != null)
+            Expanded(
+              child: child,
+            ),
+        ],
+      ),
+    );
+  }
+
+  String capitalize(String s) => s[0].toUpperCase() + s.substring(1);
+
+  Widget _buildStatusChip(String status) {
+    IconData icon;
+
+    switch (status.toLowerCase()) {
+      case 'zaakceptowany':
+        icon = Icons.check;
+        break;
+      case 'odrzucony':
+        icon = Icons.close;
+        break;
+      case 'oczekujący':
+        icon = Icons.access_time;
+        break;
+      default:
+        icon = Icons.help_outline;
+    }
+
+    final fixStatus = capitalize(status);
+
+    return RawChip(
+      label: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            icon,
+            size: 16,
+            color: AppColors.textColor2,
+          ),
+          const SizedBox(width: 4),
+          Text(
+            fixStatus,
+            style: TextStyle(
+              fontFamily: 'Roboto',
+              fontWeight: FontWeight.w600,
+              fontSize: 12,
+              height: 1.33,
+              letterSpacing: 0.5,
+              color: AppColors.textColor2,
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: AppColors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+        side: BorderSide(
+          color: const Color(0xFFCAC4D0),
+          width: 1,
+        ),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      visualDensity: VisualDensity.compact,
+    );
+  }
+
+  Widget _buildEmployeeTags(List<String> tags) {
+    if (tags.isEmpty) {
+      return Text(
+        'Brak tagów',
+        style: TextStyle(fontSize: 14, color: AppColors.textColor2),
+      );
+    }
+
+    return Wrap(
+      spacing: 8,
+      runSpacing: 8,
+      children:
+          tags
+              .map(
+                (tag) => RawChip(
+                  label: Text(
+                    tag,
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w600,
+                      fontSize: 12,
+                      height: 1.33,
+                      letterSpacing: 0.5,
+                      color: AppColors.textColor2,
+                    ),
+                  ),
+                  backgroundColor: AppColors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    side: const BorderSide(color: Color(0xFFCAC4D0), width: 1),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 6,
+                  ),
+                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  visualDensity: VisualDensity.compact,
+                ),
+              )
+              .toList(),
+    );
+  }
+
 }
